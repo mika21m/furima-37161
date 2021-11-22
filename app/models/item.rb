377d331b8f9438d89574673ab8116824fbd547bb
mika21m <93243570+mika21m@users.noreply.github.com>
2021-11-22 23:15:08 +0900

    モデルにバリデーションを設定

---
+++ app/models/item.rb
@@ -0,0 +1,18 @@
+class Item < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :shipping_cost_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :shipping_prefecture_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/ }, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+
+  belongs_to :user
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_cost
+  belongs_to :shipping_prefecture
+  belongs_to :shipping_day
+  has_one_attached :image
+end
