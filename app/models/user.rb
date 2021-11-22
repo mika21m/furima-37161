4c663df1bdb50dfde34a2d286ab0bcb8ac91d5eb
mika21m <93243570+mika21m@users.noreply.github.com>
2021-11-22 14:55:29 +0900

    passwordのバリデーション編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,5 @@ class User < ApplicationRecord
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
   validates :birthday, presence: true
-  validatable :encrypted_password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
 end
