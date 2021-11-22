da386755c8875a104d0f8ba24dd8ca9e93c71178
mika21m <93243570+mika21m@users.noreply.github.com>
2021-11-22 16:27:25 +0900

    Rubocop実行

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,8 +5,7 @@ RSpec.describe User, type: :model do
     @user = FactoryBot.build(:user)
   end
 
-  describe "ユーザー新規登録" do
-
+  describe 'ユーザー新規登録' do
     context '新規登録できるとき' do
       it 'nickname、email、password、password_confirmation、last_name、first_name、last_name_kana、first_name_kana、birthdayが存在すれば登録できる' do
         expect(@user).to be_valid
@@ -14,19 +13,19 @@ RSpec.describe User, type: :model do
     end
 
     context '新規登録できないとき' do
-      it "nicknameが空だと登録できない" do
+      it 'nicknameが空だと登録できない' do
         @user.nickname = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Nickname can't be blank")
       end
 
-      it "emailが空では登録できない" do
+      it 'emailが空では登録できない' do
         @user.email = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Email can't be blank")
       end
-      
-      it "重複したemailが存在する場合登録できない" do
+
+      it '重複したemailが存在する場合登録できない' do
         @user.save
         another_user = FactoryBot.build(:user)
         another_user.email = @user.email
@@ -34,106 +33,105 @@ RSpec.describe User, type: :model do
         expect(another_user.errors.full_messages).to include('Email has already been taken')
       end
 
-      it "emailが＠を含んでいないと登録できない" do
+      it 'emailが＠を含んでいないと登録できない' do
         @user.email = 'testtest.com'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Email is invalid")
+        expect(@user.errors.full_messages).to include('Email is invalid')
       end
 
-      it "passwordが空では登録できない" do
+      it 'passwordが空では登録できない' do
         @user.password = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Password can't be blank")
       end
 
-      it "passwordが6文字以上でないと登録できない" do
+      it 'passwordが6文字以上でないと登録できない' do
         @user.password = '00000'
         @user.password_confirmation = '00000'
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
 
-      it "passwordが半角英数字混合でないと登録できない(英字のみ)" do
+      it 'passwordが半角英数字混合でないと登録できない(英字のみ)' do
         @user.password = 'abcdef'
         @user.password_confirmation = 'abcdef'
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is invalid')
       end
 
-      it "passwordが半角英数字混合でないと登録できない(数字のみ)" do
+      it 'passwordが半角英数字混合でないと登録できない(数字のみ)' do
         @user.password = '123456'
         @user.password_confirmation = '123456'
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is invalid')
       end
 
-      it "passwordが半角でないと登録できない" do
+      it 'passwordが半角でないと登録できない' do
         @user.password = 'ＡＢＣ１２３'
         @user.password_confirmation = 'ＡＢＣ１２３'
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is invalid')
       end
 
-      it "passwordとpassword_confirmationが不一致では登録できない" do
+      it 'passwordとpassword_confirmationが不一致では登録できない' do
         @user.password_confirmation = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
 
-      it "last_nameが空だと登録できない" do
-        @user.last_name = '' 
+      it 'last_nameが空だと登録できない' do
+        @user.last_name = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Last name can't be blank")
       end
 
-      it "first_nameが空だと登録できない" do
-        @user.first_name = '' 
+      it 'first_nameが空だと登録できない' do
+        @user.first_name = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("First name can't be blank")
       end
 
-      it "last_nameが全角文字（漢字・ひらがな・カタカナ）でないと登録できない" do
-        @user.last_name = 'name' 
+      it 'last_nameが全角文字（漢字・ひらがな・カタカナ）でないと登録できない' do
+        @user.last_name = 'name'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Last name is invalid")
+        expect(@user.errors.full_messages).to include('Last name is invalid')
       end
 
-      it "first_nameが全角文字（漢字・ひらがな・カタカナ）でないと登録できない" do
-        @user.first_name = 'name' 
+      it 'first_nameが全角文字（漢字・ひらがな・カタカナ）でないと登録できない' do
+        @user.first_name = 'name'
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name is invalid")
+        expect(@user.errors.full_messages).to include('First name is invalid')
       end
 
-      it "last_name_kanaが空だと登録できない" do
-        @user.last_name_kana = '' 
+      it 'last_name_kanaが空だと登録できない' do
+        @user.last_name_kana = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Last name kana can't be blank")
       end
 
-      it "first_name_kanaが空だと登録できない" do
-        @user.first_name_kana = '' 
+      it 'first_name_kanaが空だと登録できない' do
+        @user.first_name_kana = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("First name kana can't be blank")
       end
 
-      it "last_name_kanaが全角カタカナでないと登録できない" do
-        @user.last_name_kana = 'てすと' 
+      it 'last_name_kanaが全角カタカナでないと登録できない' do
+        @user.last_name_kana = 'てすと'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Last name kana is invalid")
+        expect(@user.errors.full_messages).to include('Last name kana is invalid')
       end
 
-      it "first_name_kanaが全角カタカナでないと登録できない" do
-        @user.first_name_kana = 'てすと' 
+      it 'first_name_kanaが全角カタカナでないと登録できない' do
+        @user.first_name_kana = 'てすと'
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name kana is invalid")
+        expect(@user.errors.full_messages).to include('First name kana is invalid')
       end
 
-      it "birthdayが空だと登録できない" do
-        @user.birthday = '' 
+      it 'birthdayが空だと登録できない' do
+        @user.birthday = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Birthday can't be blank")
       end
     end
-
   end
 end
