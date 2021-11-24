5757b115fecded776cd78006584a59d228f04687
mika21m <93243570+mika21m@users.noreply.github.com>
2021-11-24 21:37:43 +0900

    半角数字のバリデーション削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,7 @@ class Item < ApplicationRecord
   validates :shipping_cost_id, numericality: { other_than: 1 , message: "can't be blank" }
   validates :shipping_prefecture_id, numericality: { other_than: 1 , message: "can't be blank" }
   validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank" }
-  validates :price, presence: true, format: { with: /\A[0-9]+\z/ }, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   validates :image, presence: true
 
   has_one_attached :image
