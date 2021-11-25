8a20e447576042db0d6e4f4f05c2430bab151d65
mika21m <93243570+mika21m@users.noreply.github.com>
2021-11-25 17:45:49 +0900

    コントローラー設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, only: :edit
+
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -8,8 +10,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_params)
-    # create = new + save
+    @item = Item.new(item_params) 
     if @item.save
       redirect_to root_path
     else
@@ -21,10 +22,32 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :image, :description, :category_id, :status_id, :shipping_cost_id,
                                  :shipping_prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless current_user.id == @item.user_id
+      redirect_to action: :index
+    end
+  end
+
 end
