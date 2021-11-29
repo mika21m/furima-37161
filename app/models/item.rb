cba402681ae713d286914c6626a0e7b5182a6ddd
mika21m <93243570+mika21m@users.noreply.github.com>
2021-11-29 16:09:18 +0900

    アソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,6 +12,7 @@ class Item < ApplicationRecord
 
   has_one_attached :image
   belongs_to :user
+  has_one :purchase_record
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
---
+++ app/models/purchase_record.rb
@@ -0,0 +1,5 @@
+class PurchaseRecord < ApplicationRecord
+  has_one :destination
+  belongs_to :item
+  belongs_to :user
+end
